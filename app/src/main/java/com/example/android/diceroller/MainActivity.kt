7d495a0760bd0c59cbe81d5066ad3916d6b9defd
Jenn <reddunrogue@gmail.com>
2021-06-22 17:54:26 -0600

    Added code for 2nd lesson and did coding challenge with two dice.

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,27 +3,45 @@ package com.example.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+    lateinit var diceImage2 : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
-        val countUpButton: Button = findViewById(R.id.count_up_button)
-        countUpButton.setOnClickListener { countUp() }
+        //val countUpButton: Button = findViewById(R.id.count_up_button)
+       // countUpButton.setOnClickListener { countUp() }
     }
 
     private fun rollDice() {
         //Toast.makeText(this, "button clicked",
          //   Toast.LENGTH_SHORT).show()
-        val randomInt = (1..6).random()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
     }
 
+    private fun getRandomDiceImage() : Int {
+        var randomInt = (1..6).random()
+        var drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        return drawableResource
+    }
+    /*
     private fun countUp() {
         val resultText: TextView = findViewById(R.id.result_text)
         val countStr = resultText.text.toString()
@@ -33,5 +51,5 @@ class MainActivity : AppCompatActivity() {
                 resultText.text = (countStr.toInt() + 1).toString()
             }
         }
-    }
+    }*/
 }
